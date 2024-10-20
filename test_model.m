0f3858fbf3549173787851e700a8a1169309f92c
Rishikesh Prabhu <110054654+Rishikesh4089@users.noreply.github.com>
2024-10-20 22:11:07 +0530

    Update test_model.m

--- test_model.m
+++ test_model.m
@@ -1,10 +1,22 @@
+% Load data and variables
 load('models/deepfake_detection_model.mat', 'trainedNet');
-imageFolder = 'test_images/';
+imageFolder = 'test_images/';  % Make sure this folder contains both the real and deepfake images
 testImages = imageDatastore(imageFolder, 'IncludeSubfolders', false, 'LabelSource', 'none');
-inputSize = [224 224 3];  % ResNet-50 expects 224x224 RGB images
-testImages.ReadFcn = @(filename)imresize(imread(filename), inputSize(1:2));
+inputSize = [224 224 3]; 
+testImages.ReadFcn = @(filename) imresize(imread(filename), inputSize(1:2));
 predictions = classify(trainedNet, testImages);
-for i = 1:length(testImages.Files)
-    [~, imageName, ext] = fileparts(testImages.Files{i});
-    fprintf('Image %s%s is classified as: %s\n', imageName, ext, string(predictions(i)));
+numImages = length(testImages.Files);
+
+% Initialise table and display image and classification
+figure('Name', 'Deepfake Detection Results', 'NumberTitle', 'off');
+sgtitle('Deepfake Detection Results');
+for i = 1:numImages
+    subplot(numImages, 2, (i-1)*2 + 1); 
+    originalImage = imread(testImages.Files{i});
+    imshow(originalImage);
+    title('Original Image');
+    subplot(numImages, 2, (i-1)*2 + 2);
+    text(0.5, 0.5, ['Classification: ', string(predictions(i))], ...
+        'FontSize', 12, 'HorizontalAlignment', 'center');
+    axis off; % Turn off the axis for the text plot
 end
